b5bf2947af6221a4889e6150d3bc7621ac9e64d5
Samuel Tremblay <samuel@sylvaintremblay.com>
2019-03-28 14:32:44 -0400

    TP9

--- src/app/shared/longueur-minimum/longueur-minimum.component.ts
+++ src/app/shared/longueur-minimum/longueur-minimum.component.ts
@@ -3,7 +3,7 @@ import { ValidatorFn, AbstractControl } from '@angular/forms';
 export class longueurMin {
     static plage(longueur:number): ValidatorFn {
         return (c: AbstractControl): { [key:string]: boolean } | null => {
-            if (c.value && c.value.trim().lenght >= longueur ) {
+            if (c.value && c.value.trim().length >= longueur ) {
                 return null;
             }
             return { 'plageInvalide': true };
